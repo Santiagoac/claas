05144af959b994d3156db78a0a205db4d627cd42
Santiago <santiagoaceves93@gmail.com>
2024-11-07 02:48:30 -0600

    update first section

--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -1,34 +1,10 @@
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
-import { Tab } from '@headlessui/react'
-import clsx from 'clsx'
-
 import { Container } from '@/components/Container'
-import backgroundImage from '@/images/background-features.jpg'
-import screenshotPayroll from '@/images/screenshots/services.png'
-import screenshotReporting from '@/images/screenshots/distribuitors.png'
-import screenshotVatReturns from '@/images/screenshots/bnpl.png'
-
-const features = [
-  {
-    title: 'Pago de servicios recurrentes',
-    description:
-      "Cambia tu factua mensual por una linea de crédito para tus clientes.",
-    image: screenshotPayroll,
-  },
-  {
-    title: 'Crédito a distribuidores',
-    description:
-      "Consesiona productos con la seguridad de una línea de crédito.",
-    image: screenshotVatReturns,
-  },
-  {
-    title: 'Parcializa compras de tus clientes en crédito simple',
-    description:
-      'Ofrece pagos a parcialidades, respaldado por un contrato de crédito.',
-    image: screenshotReporting,
-  },
-]
+import integration from '@/images/screenshots/integration.webp'
+import interests from '@/images/screenshots/interests.webp'
+import inclusion from '@/images/screenshots/inclusion.webp'
+import guarantees from '@/images/screenshots/guarantees.webp'
 
 export function PrimaryFeatures() {
   let [tabOrientation, setTabOrientation] = useState('horizontal')
@@ -49,99 +25,83 @@ export function PrimaryFeatures() {
   }, [])
 
   return (
-    <section
-      id="features"
-      aria-label="Features for running your books"
-      className="relative overflow-hidden bg-sky-600 pb-28 pt-20 sm:py-32"
-    >
-      <Image
-        className="absolute left-1/2 top-1/2 max-w-none translate-x-[-44%] translate-y-[-42%]"
-        src={backgroundImage}
-        alt=""
-        width={2245}
-        height={1636}
-        unoptimized
-      />
-      <Container className="relative">
-        <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
-          <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
-          Contratos de crédito, tu nuevo contrato comercial
-          </h2>
-          <p className="mt-6 text-lg tracking-tight text-blue-100">
-            Estructura y administra créditos para tu negocio
+    <div className="bg-gray-800 py-24 sm:py-32">
+      <Container>
+        <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="text-base/7 font-semibold text-teal-400">Reduce el riesgo</h2>
+          <p className="mt-2 max-w-lg text-pretty text-4xl font-semibold tracking-tight text-white sm:text-5xl">
+            Contratos de credito &ne; Contratos comerciales
           </p>
-        </div>
-        <Tab.Group
-          as="div"
-          className="mt-16 grid grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 md:mt-20 lg:grid-cols-12 lg:pt-0"
-          vertical={tabOrientation === 'vertical'}
-        >
-          {({ selectedIndex }) => (
-            <>
-              <div className="-mx-4 flex overflow-x-auto pb-4 sm:mx-0 sm:overflow-visible sm:pb-0 lg:col-span-5">
-                <Tab.List className="relative z-10 flex gap-x-4 whitespace-nowrap px-4 sm:mx-auto sm:px-0 lg:mx-0 lg:block lg:gap-x-0 lg:gap-y-1 lg:whitespace-normal">
-                  {features.map((feature, featureIndex) => (
-                    <div
-                      key={feature.title}
-                      className={clsx(
-                        'group relative rounded-full px-4 py-1 lg:rounded-l-xl lg:rounded-r-none lg:p-6',
-                        selectedIndex === featureIndex
-                          ? 'bg-white lg:bg-white/10 lg:ring-1 lg:ring-inset lg:ring-white/10'
-                          : 'hover:bg-white/10 lg:hover:bg-white/5'
-                      )}
-                    >
-                      <h3>
-                        <Tab
-                          className={clsx(
-                            'font-display text-lg [&:not(:focus-visible)]:focus:outline-none',
-                            selectedIndex === featureIndex
-                              ? 'text-blue-600 lg:text-white'
-                              : 'text-blue-100 hover:text-white lg:text-white'
-                          )}
-                        >
-                          <span className="absolute inset-0 rounded-full lg:rounded-l-xl lg:rounded-r-none" />
-                          {feature.title}
-                        </Tab>
-                      </h3>
-                      <p
-                        className={clsx(
-                          'mt-2 hidden text-sm lg:block',
-                          selectedIndex === featureIndex
-                            ? 'text-white'
-                            : 'text-blue-100 group-hover:text-white'
-                        )}
-                      >
-                        {feature.description}
-                      </p>
-                    </div>
-                  ))}
-                </Tab.List>
+          <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
+            <div className="flex p-px lg:col-span-4">
+              <div className="w-full overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]">
+              <Image
+                  alt="Reporting"
+                  src={integration}
+                  className="h-80 w-full object-cover object-left"
+                />
+                <div className="p-10">
+                  <h3 className="text-sm/4 font-semibold text-cyan-400">Reporta a tus clientes</h3>
+                  <p className="mt-2 text-lg font-medium tracking-tight text-white">Integra el Comportamiento de Pago a SICs</p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-400">
+                  Ayuda a tus clientes a construir su historial crediticio mientras reduces el riesgo en tus operaciones.
+                  </p>
+                </div>
               </div>
-              <Tab.Panels className="lg:col-span-7">
-                {features.map((feature) => (
-                  <Tab.Panel key={feature.title} unmount={false}>
-                    <div className="relative sm:px-6 lg:hidden">
-                      <div className="absolute -inset-x-4 bottom-[-4.25rem] top-[-6.5rem] bg-white/10 ring-1 ring-inset ring-white/10 sm:inset-x-0 sm:rounded-t-xl" />
-                      <p className="relative mx-auto max-w-2xl text-base text-white sm:text-center">
-                        {feature.description}
-                      </p>
-                    </div>
-                    <div className="mt-10 w-[45rem] overflow-hidden rounded-xl bg-slate-50 shadow-xl shadow-blue-900/20 sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
-                      <Image
-                        className="w-full"
-                        src={feature.image}
-                        alt=""
-                        priority
-                        sizes="(min-width: 1024px) 67.8125rem, (min-width: 640px) 100vw, 45rem"
-                      />
-                    </div>
-                  </Tab.Panel>
-                ))}
-              </Tab.Panels>
-            </>
-          )}
-        </Tab.Group>
+            </div>
+            <div className="flex p-px lg:col-span-2">
+              <div className="w-full overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 lg:rounded-tr-[2rem]">
+              <Image
+                  alt="Interests"
+                  src={interests}
+                  className="h-80 w-full object-cover object-center"
+                />
+                <div className="p-10">
+                  <h3 className="text-sm/4 font-semibold text-cyan-400">Aumenta tus ganancias</h3>
+                  <p className="mt-2 text-lg font-medium tracking-tight text-white">Cobra Intereses Ordinarios y Moratorios</p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-400">
+                    Genera ingresos adicionales aplicando intereses ordinarios y moratorios que optimizan tus retornos.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex p-px lg:col-span-2">
+              <div className="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 lg:rounded-bl-[2rem] w-full">
+              <Image
+                  alt="Garantees"
+                  src={guarantees}
+                  width={640}
+                  height={320}
+                  className="h-80 w-full object-cover object-left"
+                />
+                <div className="p-10">
+                  <h3 className="text-sm/4 font-semibold text-cyan-400">Mejora tus garantías</h3>
+                  <p className="mt-2 text-lg font-medium tracking-tight text-white">Refuerza la Seguridad de tus Contratos</p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-400">
+                    Protege cada operación con contratos diseñados para asegurar el cumplimiento y garantizar el flujo de efectivo.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex p-px lg:col-span-4">
+              <div className="w-full overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]">
+              <Image
+                  alt="Inclusion"
+                  src={inclusion}
+                  className="h-80 w-full object-cover object-center"
+                />
+                <div className="p-10">
+                <h3 className="text-sm/4 font-semibold text-cyan-400">Créditos Accesibles para Todos</h3>
+                  <p className="mt-2 text-lg font-medium tracking-tight text-white">Impulsa la Inclusión Financiera</p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-400">
+                    Abre nuevas oportunidades con productos de crédito flexibles que llegan a una amplia gama de perfiles.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </Container>
-    </section>
+    </div>
   )
 }
